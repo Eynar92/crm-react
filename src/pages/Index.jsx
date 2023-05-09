cae4310a698bb417be3bd3ce7a968d52f6c4f922
Eynar Alvarez <eynar.j.alvarez.m@gmail.com>
2023-05-09 00:02:10 -0400

    The function to delete costumers information was implemented

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -18,24 +18,27 @@ function Index() {
       <p className="mt-3">Administra tus Clientes</p>
 
       {clientes.length ? (
-        <table className="w-full bg-white shadow mt-5 table-auto">
-          <thead className="bg-blue-800 text-white">
-            <tr>
-              <th className="p-2">Cliente</th>
-              <th className="p-2">Contacto</th>
-              <th className="p-2">Acciones</th>
-            </tr>
-          </thead>
+        <div className="overflow-auto">
+          <table className="w-full bg-white shadow mt-5 table-auto">
+            <thead className="bg-blue-800 text-white">
+              <tr>
+                <th className="p-2">Cliente</th>
+                <th className="p-2">Contacto</th>
+                <th className="p-2">Acciones</th>
+              </tr>
+            </thead>
 
-          <tbody>
-            {clientes.map(cliente => (
-              <Cliente
-                cliente={cliente}
-                key={cliente.id}
-              />
-            ))}
-          </tbody>
-        </table>
+            <tbody>
+              {clientes.map(cliente => (
+                <Cliente
+                  cliente={cliente}
+                  key={cliente.id}
+                />
+              ))}
+            </tbody>
+          </table>
+        </div>
+
       ) : (
         <p className="text-center mt-10">No hay clientes aún</p >
       )
